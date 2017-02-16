948744f5e81c63f72059e2790ad81c574c523ff6
Avinash Nilmadhub <a.nilmadhub@mediacall.mu>
2017-02-16 14:32:15 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Grocery App';
+  newItem : groceryItem;
+  items: FirebaseListObservable<any[]>;
+
+  /**
+   * constructor method to initialize all variables
+   * and to fetch the grocery list from firebase json object
+   */
+  constructor(private af: AngularFire){
+    this.items = af.database.list('/items');
+    this.newItem = {
+      product: '',
+      quantity: 1,
+      inBasket: false
+    };
+  }
+
+  /**
+   * Function to add items to the grocery list
+   */
+  addItemToList() {
+    let product = this.newItem.product;
+    let inBasket = this.newItem.inBasket;
+    let quantity = this.newItem.quantity;
+    this.af.database.object(`/items/${product}`).set({ inBasket : inBasket, quantity : quantity });
+    
+    //reinitialise the item variable
+    this.newItem = {
+      product: '',
+      quantity: 1,
+      inBasket: false
+    };
+  }
+
+  /**
+   * Update status of item when it is placed in basket
+   */
+  updateItem(product, inBasket) {
+    inBasket = (inBasket)? false :true;
+    this.af.database.object(`/items/${product}`).update({ inBasket : inBasket });
+  }
+
+  /**
+   * Function to delete item from the list
+   */
+  removeItem(product){
+    this.af.database.object(`/items/${product}`).remove();
+  }
+
+}
+
+interface groceryItem {
+  product: string;
+  quantity: number;
+  inBasket: Boolean;
 }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAFnDmGAfEKYwXdH3pJocssmM539tG6eIM",
+    authDomain: "grocery-app-2c7f1.firebaseapp.com",
+    databaseURL: "https://grocery-app-2c7f1.firebaseio.com",
+    storageBucket: "grocery-app-2c7f1.appspot.com",
+    messagingSenderId: "171008144760"
+};
